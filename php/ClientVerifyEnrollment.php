01e83571f57043743b7d9cacdc7f70338b09d8c2
berganza14 <inniber@gmail.com>
2020-11-23 18:04:23 +0100

    empezando lab7

--- php/ClientVerifyEnrollment.php
+++ php/ClientVerifyEnrollment.php
@@ -18,14 +18,10 @@
       //donde se encuentra el servicio SOAP que vamos a utilizar.
 
       //URL BUENA -> http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl
-      $soapclient = new nusoap_client( 'http://localhost/nusoap-0.9.5/samples/comprobarmatricula.php?wsdl',
-      true);
-      //Llamamos la función que habíamos implementado en el Web Service
-      //e imprimimos lo que nos devuelve
+      $soapclient = new nusoap_client('https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl', true);
       if (isset($_POST['email']))
       {
-        echo '<h1>La suma es: ' . $soapclient->call('sumar',array( 'x'=>$_POST['sumando1'],
-        'y'=>$_POST['sumando2'])). '</h1>';
+        echo '<h1>El cliente es: ' . $soapclient->call('comprobarmatricula',array( 'x'=>$_POST['email']))'</h1>';
         echo '<h2>Request</h2><pre>' . htmlspecialchars($soapclient->request, ENT_QUOTES) . '</pre>';
         echo '<h2>Response</h2><pre>' . htmlspecialchars($soapclient->response, ENT_QUOTES) . '</pre>';
         echo '<h2>Debug</h2>';
---
+++ php/VerifyPassWS.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../lib/nusoap.php');
+require_once('../lib/class.wsdlcache.php');
+
+$ns="http://localhost/nusoap-0.9.5/samples";
+$server = new soap_server;
+$server -> configureWSDL('verificar',$ns);
+
+$server->wsdl->schemaTargetNamespace=$ns;
+
+$server->register('verificar',array('x'=>'xsd:string'),$ns);
+
+function verificar($x){
+
+	$handle = fopen('../txt/toppasswords.txt', 'r');
+	$valid = false;
+	while (($buffer = fgets($handle)) !== false) {
+    	if (strpos($buffer, $id) !== false) {
+        	$valid = TRUE;
+        	break;
+    	}      
+	}
+	fclose($handle);
+	return $valid; 
+
+}
+
+if(!isset($HTTP_RAW_POST_DATA)) $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+$server -> service($HTTP_RAW_POST_DATA);
+?>
